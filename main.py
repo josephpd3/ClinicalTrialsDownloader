92e4bf52032e7c46539e7fe176aabbe010ae326c
Joseph P DeBartola III <joe@codeofjoe.com>
2015-11-11 06:53:14 -0600

    Got rid of senseless abstraction in Main class in main.py. Now replaced with simple functions.

--- main.py
+++ main.py
@@ -38,125 +38,134 @@ EXTRACT_PATH = os.path.join(BASE_PATH, 'extracted')
 DOWNLOAD_PATH = os.path.join(BASE_PATH, 'downloads')
 
 
-class Main(object):
+def main(argv):
     """
-    Encapsulated main execution script.
+    Encapsulating main execution script.
+    """
+    search_terms = parse_args(argv)
+    download_dirs = download_research(search_terms)
+    extract_research(download_dirs)
+
+
+def parse_args(argv):
+    """
+    Parse command line/terminal parameters.
+
+    Returns search terms for downloading results.
     """
     terms = None
-    trials_downloader = None
+    try:
+        opts, args = getopt.getopt(argv, '', OPTION_LIST)
+    except getopt.GetoptError:
+        raise getopt.GetoptError(
+            '> Command Line options available are as follows:\n\n' +
+            '\t--explicit-terms=multi+word+term_term\n\t\t' +
+            'Explicitly define terms as opposed to using params.txt.'
+        )
 
-    def __init__(self, argv):
-        """
-        Parse command line/terminal parameters and run accordingly.
-        """
+    for option in opts:
+        if option[0] == '--explicit-terms':
+            terms = option[1]
+
+    return terms
+
+
+def download_research(terms):
+    """
+    Creates an instance of TrialsDownloader class
+    and downloads trial records relevant to
+    specified criteria.
+
+    Returns relative folder names containing downloaded XML files
+    """
+    trials_downloader = TrialsDownloader(terms=terms)
+    try:
+        trials_downloader.make_sure_path_exists(EXTRACT_PATH)
+    except OSError:
+        raise OSError(
+            """
+            Could not write dataframe to file.
+            Are the permissions for /extracted/ set
+            properly for file writing?
+            """
+        )
+    return trials_downloader.relative_download_dirs
+
+
+def extract_research(relative_dirs):
+    """
+    Extracts trial records from XML files and
+    exports it to Dataframes which are concatenated
+    into one large dataframe and then saved as
+    pickle (.pkl) files, which is probably
+    the neatest way to store DataFrames.
+    """
+    xtdf = XMLToDataFrame()
+    df_saved = False
+
+    research_directories = [os.path.join(DOWNLOAD_PATH, d)
+                            for d in relative_dirs]
+
+    for rdir in research_directories:
+        rdir_full_path = rdir
+        dir_name = os.path.basename(rdir)
+
+        print('> Collecting extracted XML files...')
+
+        files = get_xml_file_list(rdir)
+
+        print('> Creating DataFrames for {}'.format(dir_name))
+
+        dataframes = []
+
+        for f in files:
+            xtdf.parse_xml_file(f)
+
+            df = xtdf.to_dataframe()
+            dataframes.append(df)
+
+        combined_dataframe = pd.concat(dataframes)
+
         try:
-            opts, args = getopt.getopt(argv, '', OPTION_LIST)
-        except getopt.GetoptError:
-            raise getopt.GetoptError(
-                '> Command Line options available are as follows:\n\n' +
-                '\t--explicit-terms=multi+word+term_term\n\t\t' +
-                'Explicitly define terms as opposed to using params.txt.'
+            pickle_name = dir_name + '.pkl'
+            combined_dataframe.to_pickle(
+                os.path.join(EXTRACT_PATH, pickle_name)
             )
-
-        for option in opts:
-            if option[0] == '--explicit-terms':
-                self.terms = option[1]
-
-        self.download_research()
-        self.extract_research()
-
-    def download_research(self):
-        """
-        Creates an instance of TrialsDownloader class
-        and downloads trial records relevant to
-        specified criteria.
-        """
-        self.trials_downloader = TrialsDownloader(terms=self.terms)
-        try:
-            self.trials_downloader.make_sure_path_exists(EXTRACT_PATH)
-        except OSError:
-            raise OSError(
+        except Exception:
+            raise IOError(
                 """
                 Could not write dataframe to file.
                 Are the permissions for /extracted/ set
                 properly for file writing?
                 """
             )
+        else:
+            df_saved = True
+            shutil.rmtree(rdir_full_path)
 
-    def extract_research(self):
-        """
-        Extracts trial records from XML files and
-        exports it to Dataframes which are concatenated
-        into one large dataframe and then saved as
-        pickle (.pkl) files, which is probably
-        the neatest way to store DataFrames.
-        """
-        xtdf = XMLToDataFrame()
-        df_saved = False
+    print('> Clearing out downloads folder...')
 
-        research_directories = [os.path.join(DOWNLOAD_PATH, d)
-                                for d in
-                                self.trials_downloader.relative_download_dirs]
+    if df_saved:
+        print('> Dataframes saved!')
 
-        for rdir in research_directories:
-            rdir_full_path = rdir
-            dir_name = os.path.basename(rdir)
 
-            print('> Collecting extracted XML files...')
+def get_xml_file_list(dir_name):
+    """
+    Get paths to xml files given a
+    containing directory.
+    """
+    xml_files = []
 
-            files = self.get_xml_file_list(rdir)
+    xml_pat = re.compile('\.xml$')
 
-            print('> Creating DataFrames for {}'.format(dir_name))
+    files = [os.path.join(dir_name, xf)
+             for xf
+             in os.listdir(dir_name)
+             if xml_pat.search(xf) is not None]
+    xml_files.extend(files)
 
-            dataframes = []
-
-            for f in files:
-                xtdf.parse_xml_file(f)
-
-                df = xtdf.to_dataframe()
-                dataframes.append(df)
-
-            combined_dataframe = pd.concat(dataframes)
-
-            try:
-                pickle_name = dir_name + '.pkl'
-                combined_dataframe.to_pickle(
-                    os.path.join(EXTRACT_PATH, pickle_name)
-                )
-            except Exception:
-                raise IOError(
-                    """
-                    Could not write dataframe to file.
-                    Are the permissions for /extracted/ set
-                    properly for file writing?
-                    """
-                )
-            else:
-                df_saved = True
-                shutil.rmtree(rdir_full_path)
-
-        print('> Clearing out downloads folder...')
-
-        if df_saved:
-            print('> Dataframes saved!')
-
-    def get_xml_file_list(self, dir_name):
-        """
-        Get paths to xml files given a
-        containing directory.
-        """
-        xml_files = []
-
-        xml_pat = re.compile('\.xml$')
-
-        files = [os.path.join(dir_name, xf)
-                 for xf
-                 in os.listdir(dir_name)
-                 if xml_pat.search(xf) is not None]
-        xml_files.extend(files)
-
-        return xml_files
+    return xml_files
 
 
 if __name__ == '__main__':
-    main = Main(sys.argv[1:])
+    main(sys.argv[1:])
